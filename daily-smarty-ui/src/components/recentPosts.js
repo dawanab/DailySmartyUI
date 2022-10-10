1b9dfdcc256457d7a3fab73964bf2560cee1e5b1
Dawana Baker <krunk_011@hotmail.com>
2022-10-09 22:47:02 -0500

    set redux thunk & connect RecentPosts to redux

--- daily-smarty-ui/src/components/recentPosts.js
+++ daily-smarty-ui/src/components/recentPosts.js
@@ -1,7 +1,14 @@
-import React, { Component } from "react"
-import SearchBar from "./searchBar"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import SearchBar from "./searchBar";
 
 class RecentPosts extends Component {
+
+	componentDidMount() {
+		this.props.fetchRecentPosts();
+	}
 	render() {
 		return(
 			<div className="recent-posts">
@@ -18,4 +25,4 @@ class RecentPosts extends Component {
 	}
 }
 
-export default RecentPosts;
+export default connect(null, actions)(RecentPosts);
