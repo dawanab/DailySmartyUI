a4fd80cc2e9968a47858acbec0b29db48462ba47
Dawana Baker <krunk_011@hotmail.com>
2022-10-10 15:41:27 -0500

    utilize react animate height npm package

--- daily-smarty-ui/src/components/home.js
+++ daily-smarty-ui/src/components/home.js
@@ -1,31 +1,84 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
 
-import * as actions from "../actions";
 import Logo from './logo';
 import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Home extends Component {
 
-	handleSearchBarSubmit(query) {
-		this.props.fetchPostsWithQuery(query);
-		this.props.history.push("/results");
-	}
+  handleSearchBarSubmit(query) {
+      this.props.fetchPostsWithQuery(query, () => {
+        this.props.history.push('/results');
+      });
 
-	render() {
-		return (
-			<div>
-				<div>
-					<Logo />
-					<SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
-					<RecentPosts />
-				</div>
-			</div>
-		);
-	}
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Logo/>
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+          <RecentPosts/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(Home);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { connect } from "react-redux";
+
+// import * as actions from "../actions";
+// import Logo from './logo';
+// import SearchBar from './searchBar';
+// import RecentPosts from './recentPosts';
+
+
+// class Home extends Component {
+
+// 	handleSearchBarSubmit(query) {
+// 		this.props.fetchPostsWithQuery(query);
+// 		this.props.history.push("/results");
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<div>
+// 					<Logo />
+// 					<SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+// 					<RecentPosts />
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default connect(null, actions)(Home);
 
--- daily-smarty-ui/src/components/post.js
+++ daily-smarty-ui/src/components/post.js
@@ -1,57 +1,146 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
 
-	renderTopics() {
-		let topics = this.props.associated_topics.map((topic, index) => {
-			return <span className="post-topic" key={index}>{topic}</span>
-		})
-		return topics;
-	}
+    constructor(props) {
+        super(props)
 
-	renderLinks() {
-			let links = this.props.post_links.map((post_link, index) => {
-					return (
-							<div className="post-link" key={index}>
-									<div className="post-link__box"></div>
-									<div className="post-link__link">
-										<a href={post_link.link_url}>Useful Link #{index + 1}</a>
-									</div>
-							</div>
-					)
-			})
-			return links;
-	}
+        this.state = {
+            height: 0
+        }
+    }
 
-	render() {
-			if(this.props.type == 'recent') {
-					return (
-							<li className="recent-post">
-									<div className="recent-post_title">
-											{this.props.title}
-									</div>
-									<div className="recent-post_topics">
-											{this.renderTopics()}
-									</div>
-							</li>
-					)
-			} else if(this.props.type == 'result') {
-					return (
-							<li className="result-post">
-									<div className="result-post_topics">
-											{this.renderTopics()}
-									</div>
-									<div className="result-post_title">
-											{this.props.title}
-									</div>
-									<div className="result-post_links">
-											{this.renderLinks()}
-									</div>
-							</li>
-					)
-			}
+    renderTopics() {
+        let topics = this.props.associated_topics.map((topic, index) => {
+            return <span className="post-topic" key={index}>{topic}</span>
+        })
+        return topics;
+    }
 
-	}
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                    <div className="post-link__box"></div>
+                    <div className="post-link__link">
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                    </div>
+                </div>
+            )
+        })
+        return links;
+    }
+
+    render() {
+        if(this.props.type == 'recent') {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="recent-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                </li>
+            )
+        } else if(this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post__title">
+                        <a href={this.props.url_for_post}
+                           onMouseEnter={() => this.setState({ height: 70 })}
+                           onMouseLeave={() => this.setState({ height: 0 })}
+                        >
+                        {this.props.title}</a>
+                    </div>
+                    <AnimateHeight
+                        duration={500}
+                        height={this.state.height}
+                    >
+                        <div className="result-post__links">
+                            {this.renderLinks()}
+                        </div>
+                    </AnimateHeight>
+                </li>
+            )
+        }
+
+    }
 }
 
 export default Post;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+
+// class Post extends Component {
+
+// 	renderTopics() {
+// 		let topics = this.props.associated_topics.map((topic, index) => {
+// 			return <span className="post-topic" key={index}>{topic}</span>
+// 		})
+// 		return topics;
+// 	}
+
+// 	renderLinks() {
+// 			let links = this.props.post_links.map((post_link, index) => {
+// 					return (
+// 							<div className="post-link" key={index}>
+// 									<div className="post-link__box"></div>
+// 									<div className="post-link__link">
+// 										<a href={post_link.link_url}>Useful Link #{index + 1}</a>
+// 									</div>
+// 							</div>
+// 					)
+// 			})
+// 			return links;
+// 	}
+
+// 	render() {
+// 			if(this.props.type == 'recent') {
+// 					return (
+// 							<li className="recent-post">
+// 									<div className="recent-post_title">
+// 											{this.props.title}
+// 									</div>
+// 									<div className="recent-post_topics">
+// 											{this.renderTopics()}
+// 									</div>
+// 							</li>
+// 					)
+// 			} else if(this.props.type == 'result') {
+// 					return (
+// 							<li className="result-post">
+// 									<div className="result-post_topics">
+// 											{this.renderTopics()}
+// 									</div>
+// 									<div className="result-post_title">
+// 											{this.props.title}
+// 									</div>
+// 									<div className="result-post_links">
+// 											{this.renderLinks()}
+// 									</div>
+// 							</li>
+// 					)
+// 			}
+
+// 	}
+// }
+
+// export default Post;
